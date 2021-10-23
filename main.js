9d63c265927c94e5fd74ce0a89a7e7d7144b7edf
及川恵 <keitin10009@gmail.com>
2021-10-23 18:07:17 +0900

    ポートフォリオサイト作成

---
+++ main.js
@@ -0,0 +1,6 @@
+const comment = document.getElementById("comment");
+const feedback = document.getElementById("feedback");
+comment.onclick = function () {
+  feedback.textContent =
+    "あと２か月間も頑張っていきたいと思います！よろしくお願いします！";
+};
